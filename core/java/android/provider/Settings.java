2387e68c106790fa453d50f1bbf17c6398a8b339
Pulser <pulser@xda-developers.com>
2014-02-13 19:25:55 +0100

    Merge "[1/2] base: Add ADB Paranoia mode" into android-4.4
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: I1f9c9e42dcd8cbacd7e7c4bc24c6bdf55db04fc3
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6271,10 +6271,15 @@ public final class Settings {
         public static final String DEVICE_HOSTNAME = "device_hostname";
 
         /**
-         * The TCP/IP port to run ADB on, or -1 for USB
          * @hide
          */
         public static final String ADB_PORT = "adb_port";
+
+        /**
+         * Whether to reboot the device if an unknown ADB host is detected while screen is locked
+         * @hide
+         */
+        public static final String ADB_PARANOID = "adb_paranoid";
 
         /**
          * Setting to allow mock locations and location provider status to be injected into the

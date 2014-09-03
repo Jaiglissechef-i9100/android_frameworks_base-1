DirtyUnicorns/android_frameworks_base

Bring back more PIE targets [1/2]
- Replaced App window with Torch pie trigger

Change-Id: Id1d748940937e6ce905aee677da91fb304863c8b

core/java/android/provider/Settings.java
@@ -4978,6 +4978,36 @@ public final class Settings {
         public static final String PIE_CHEVRON_RIGHT = "pie_chevron_right";
 
         /**
+         * Pie last app, should default to 0 (no, show only when needed)
+         * @hide
+         */
+        public static final String PIE_LAST_APP = "pie_last_app";
+
+        /**
+         * Pie kill task, default to 0 (off)
+         * @hide
+         */
+        public static final String PIE_KILL_TASK = "pie_kill_task";
+
+        /**
+         * Pie toggles omni torch
+         * @hide
+         */
+        public static final String PIE_TORCH = "pie_torch";
+
+        /**
+         * Pie action notifications, default to off
+         * @hide
+         */
+        public static final String PIE_ACT_NOTIF = "pie_act_notif";
+
+        /**
+         * Pie power, should default to 0 (no, show only when needed)
+         * @hide
+         */
+        public static final String PIE_POWER = "pie_power";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1995,7 +1995,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         mPieEnabled = Settings.System.getInt(resolver, Settings.System.PIE_CONTROLS, 0) != 0;
         mPieGravity = Settings.System.getInt(resolver, Settings.System.PIE_GRAVITY, 3);
         mPieTriggerSize = Settings.System.getFloat(resolver, Settings.System.PIE_TRIGGER, 1f);
-        mPieStick = Settings.System.getInt(resolver, Settings.System.PIE_STICK, 1) != 0;
+        mPieStick = Settings.System.getInt(resolver, Settings.System.PIE_STICK, 0) != 0;
         mPieCenter = Settings.System.getInt(resolver, Settings.System.PIE_CENTER, 1) != 0;
 
         pieRemove();

packages/SystemUI/src/com/android/systemui/statusbar/PieControlPanel.java
@@ -16,17 +16,22 @@
 
 package com.android.systemui.statusbar;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.ActivityOptions;
 import android.app.KeyguardManager;
 import android.app.SearchManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.hardware.input.InputManager;
 import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -40,9 +45,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.android.internal.util.du.TorchConstants;
+import com.android.internal.statusbar.IStatusBarService;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.PieControl.OnNavButtonPressedListener;
+
+import java.util.List;
 
 public class PieControlPanel extends FrameLayout implements StatusBarPanel,
         OnNavButtonPressedListener {
@@ -66,6 +76,8 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
     Rect mContentArea = new Rect();
 
     private BaseStatusBar mStatusBar;
+
+    private final static String SysUIPackage = "com.android.systemui";
 
     public PieControlPanel(Context context) {
         this(context, null);
@@ -146,7 +158,7 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
 
     public void bumpConfiguration() {
         if (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.PIE_STICK, 1) != 0) {
+                Settings.System.PIE_STICK, 0) != 0) {
 
             // Get original offset
             int gravityIndex = findGravityOffset(convertPieGravitytoGravity(mStatusBar.mPieGravity));
@@ -302,6 +314,24 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
             mStatusBar.toggleRecentApps();
         } else if (buttonName.equals(PieControl.SEARCH_BUTTON)) {
             launchAssistAction();
+        } else if (buttonName.equals(PieControl.LAST_APP_BUTTON)) {
+            toggleLastApp();
+        } else if (buttonName.equals(PieControl.KILL_TASK_BUTTON)) {
+            KillTask mKillTask = new KillTask(mContext);
+            mHandler.post(mKillTask);
+        } else if (buttonName.equals(PieControl.POWER_BUTTON)) {
+            injectKeyDelayed(KeyEvent.KEYCODE_POWER);
+        } else if (buttonName.equals(PieControl.TORCH_BUTTON)) {
+            Intent torch = new Intent(TorchConstants.ACTION_TOGGLE_STATE);
+            mContext.sendBroadcast(torch);
+        } else if (buttonName.equals(PieControl.ACT_NOTIF_BUTTON)) {
+            try {
+                IStatusBarService.Stub.asInterface(
+                ServiceManager.getService(mContext.STATUS_BAR_SERVICE)).expandNotificationsPanel();
+            } catch (RemoteException e) {
+                // A RemoteException is like a cold
+                // Let's hope we don't catch one!
+            }
         }
     }
 
@@ -324,6 +354,59 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
             }
         }
     }
+
+    private void toggleLastApp() {
+        int lastAppId = 0;
+        int looper = 1;
+        String packageName;
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        final ActivityManager am = (ActivityManager) mContext
+                .getSystemService(Activity.ACTIVITY_SERVICE);
+        String defaultHomePackage = "com.android.launcher";
+        intent.addCategory(Intent.CATEGORY_HOME);
+        final ResolveInfo res = mContext.getPackageManager().resolveActivity(intent, 0);
+        if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
+            defaultHomePackage = res.activityInfo.packageName;
+        }
+        List <ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(5);
+        // lets get enough tasks to find something to switch to
+        // Note, we'll only get as many as the system currently has - up to 5
+        while ((lastAppId == 0) && (looper < tasks.size())) {
+            packageName = tasks.get(looper).topActivity.getPackageName();
+            if (!packageName.equals(defaultHomePackage) && !packageName.equals("com.android.systemui")) {
+                lastAppId = tasks.get(looper).id;
+            }
+            looper++;
+        }
+        if (lastAppId != 0) {
+            am.moveTaskToFront(lastAppId, am.MOVE_TASK_NO_USER_ACTION);
+        }
+    }
+
+    public static class KillTask implements Runnable {
+         private Context mContext;
+         public KillTask(Context context) {
+             this.mContext = context;
+         }
+         public void run() {
+            final Intent intent = new Intent(Intent.ACTION_MAIN);
+            final ActivityManager am = (ActivityManager) mContext
+                    .getSystemService(Activity.ACTIVITY_SERVICE);
+            String defaultHomePackage = "com.android.launcher";
+            intent.addCategory(Intent.CATEGORY_HOME);
+            final ResolveInfo res = mContext.getPackageManager().resolveActivity(intent, 0);
+            if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
+                defaultHomePackage = res.activityInfo.packageName;
+            }
+            String packageName = am.getRunningTasks(1).get(0).topActivity.getPackageName();
+            if (SysUIPackage.equals(packageName))
+                return; // don't kill SystemUI
+            if (!defaultHomePackage.equals(packageName)) {
+                am.forceStopPackage(packageName);
+                Toast.makeText(mContext, com.android.internal.R.string.app_killed_message, Toast.LENGTH_SHORT).show();
+            }
+        }
+     }
 
     public void injectKeyDelayed(int keycode) {
         mInjectKeycode = keycode;

packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
@@ -199,6 +199,11 @@ public class PieMenu extends FrameLayout {
     private boolean mEnableColor;
     private boolean mUseMenuAlways;
     private boolean mUseSearch;
+    private boolean mUseLastApp;
+    private boolean mUseKillTask;
+    private boolean mUseTorch;
+    private boolean mUseActNotif;
+    private boolean mUsePower;
     private boolean mHapticFeedback;
 
     // Animations
@@ -247,7 +252,17 @@ public class PieMenu extends FrameLayout {
         mUseMenuAlways = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PIE_MENU, 1) == 1;
         mUseSearch = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.PIE_SEARCH, 1) == 1;
+                Settings.System.PIE_SEARCH, 0) == 1;
+        mUseLastApp = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_LAST_APP, 0) == 1;
+        mUseKillTask = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_KILL_TASK, 0) == 1;
+        mUseTorch = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_TORCH, 0) == 1;
+        mUseActNotif = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_ACT_NOTIF, 0) == 1;
+        mUsePower = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_POWER, 0) == 1;
         mStatusMode = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PIE_MODE, 2);
         mPieSize = Settings.System.getFloat(mContext.getContentResolver(),
@@ -664,9 +679,14 @@ public class PieMenu extends FrameLayout {
     }
 
     private boolean canItemDisplay(PieItem item) {
-        return !(item.getName().equals(PieControl.MENU_BUTTON) && !mPanel.currentAppUsesMenu() && !mUseMenuAlways)
-                &&
-                !(item.getName().equals(PieControl.SEARCH_BUTTON) && !mUseSearch);
+        return !(item.getName().equals(PieControl.MENU_BUTTON) && !mPanel.currentAppUsesMenu()
+                        && !mUseMenuAlways) &&
+               !(item.getName().equals(PieControl.SEARCH_BUTTON) && !mUseSearch) &&
+               !(item.getName().equals(PieControl.ACT_NOTIF_BUTTON) && !mUseActNotif) &&
+               !(item.getName().equals(PieControl.TORCH_BUTTON) && !mUseTorch) &&
+               !(item.getName().equals(PieControl.KILL_TASK_BUTTON) && !mUseKillTask) &&
+               !(item.getName().equals(PieControl.LAST_APP_BUTTON) && !mUseLastApp) &&
+               !(item.getName().equals(PieControl.POWER_BUTTON) && !mUsePower);
     }
 
     private void layoutPie() {
@@ -678,6 +698,16 @@ public class PieMenu extends FrameLayout {
         if (!mPanel.currentAppUsesMenu() && !mUseMenuAlways)
             itemCount--;
         if (!mUseSearch)
+            itemCount--;
+        if (!mUseActNotif)
+            itemCount--;
+        if (!mUseLastApp)
+            itemCount--;
+        if (!mUseTorch)
+            itemCount--;
+        if (!mUseKillTask)
+            itemCount--;
+        if (!mUsePower)
             itemCount--;
 
         int lesserSweepCount = 0;

Jaiglissechef-i9100/android_frameworks_base-1

Conflicts:
	packages/SystemUI/res/drawable-hdpi/ic_sysbar_power.png
	packages/SystemUI/res/drawable-mdpi/ic_sysbar_power.png
	packages/SystemUI/res/drawable-xhdpi/ic_sysbar_power.png
	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
	packages/SystemUI/src/com/android/systemui/statusbar/PieControlPanel.java
	packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java

core/java/android/provider/Settings.java
@@ -5318,6 +5318,35 @@ public final class Settings {
          */
         public static final String PA_PIE_CHEVRON_RIGHT = "pa_pie_chevron_right";
 
+        /**
+         * Pie last app, should default to 0 (no, show only when needed)
+         * @hide
+         */
+        public static final String PA_PIE_LAST_APP = "pa_pie_last_app";
+
+        /**
+         * Pie kill task, default to 0 (off)
+         * @hide
+         */
+        public static final String PA_PIE_KILL_TASK = "pa_pie_kill_task";
+
+        /**
+         * Pie toggles omni torch
+         * @hide
+         */
+        public static final String PA_PIE_TORCH = "pa_pie_torch";
+
+        /**
+         * Pie action notifications, default to off
+         * @hide
+         */
+        public static final String PA_PIE_ACT_NOTIF = "pa_pie_act_notif";
+
+        /**
+         * Pie power, should default to 0 (no, show only when needed)
+         * @hide
+         */
+        public static final String PA_PIE_POWER = "pa_pie_power";
 
        /**
          * HALO enabled, should default to 0 (HALO is disabled)

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2366,7 +2366,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         mPieEnabled = Settings.System.getInt(resolver, Settings.System.PA_PIE_CONTROLS, 0) != 0;
         mPieGravity = Settings.System.getInt(resolver, Settings.System.PA_PIE_GRAVITY, 3);
         mPieTriggerSize = Settings.System.getFloat(resolver, Settings.System.PA_PIE_TRIGGER, 1f);
-        mPieStick = Settings.System.getInt(resolver, Settings.System.PA_PIE_STICK, 1) != 0;
+        mPieStick = Settings.System.getInt(resolver, Settings.System.PA_PIE_STICK, 0) != 0;
         mPieCenter = Settings.System.getInt(resolver, Settings.System.PA_PIE_CENTER, 1) != 0;
 
         pieRemove();

packages/SystemUI/src/com/android/systemui/statusbar/PieControlPanel.java
@@ -16,17 +16,22 @@
 
 package com.android.systemui.statusbar;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.ActivityOptions;
 import android.app.KeyguardManager;
 import android.app.SearchManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.hardware.input.InputManager;
 import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -40,9 +45,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.android.internal.util.du.TorchConstants;
+import com.android.internal.statusbar.IStatusBarService;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.PieControl.OnNavButtonPressedListener;
+
+import java.util.List;
 
 public class PieControlPanel extends FrameLayout implements StatusBarPanel,
         OnNavButtonPressedListener {
@@ -66,6 +76,8 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
     Rect mContentArea = new Rect();
 
     private BaseStatusBar mStatusBar;
+
+    private final static String SysUIPackage = "com.android.systemui";
 
     public PieControlPanel(Context context) {
         this(context, null);
@@ -146,7 +158,7 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
 
     public void bumpConfiguration() {
         if (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.PA_PIE_STICK, 1) != 0) {
+                Settings.System.PA_PIE_STICK, 0) != 0) {
 
             // Get original offset
             int gravityIndex = findGravityOffset(convertPieGravitytoGravity(mStatusBar.mPieGravity));
@@ -226,7 +238,7 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
                 }
             }
             Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.PIE_GRAVITY, convertGravitytoPieGravity(gravityOffset));
+                    Settings.System.PA_PIE_GRAVITY, convertGravitytoPieGravity(gravityOffset));
         }
     }
 
@@ -302,6 +314,24 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
             mStatusBar.toggleRecentApps();
         } else if (buttonName.equals(PieControl.SEARCH_BUTTON)) {
             launchAssistAction();
+        } else if (buttonName.equals(PieControl.LAST_APP_BUTTON)) {
+            toggleLastApp();
+        } else if (buttonName.equals(PieControl.KILL_TASK_BUTTON)) {
+            KillTask mKillTask = new KillTask(mContext);
+            mHandler.post(mKillTask);
+        } else if (buttonName.equals(PieControl.POWER_BUTTON)) {
+            injectKeyDelayed(KeyEvent.KEYCODE_POWER);
+        } else if (buttonName.equals(PieControl.TORCH_BUTTON)) {
+            Intent torch = new Intent(TorchConstants.ACTION_TOGGLE_STATE);
+            mContext.sendBroadcast(torch);
+        } else if (buttonName.equals(PieControl.ACT_NOTIF_BUTTON)) {
+            try {
+                IStatusBarService.Stub.asInterface(
+                ServiceManager.getService(mContext.STATUS_BAR_SERVICE)).expandNotificationsPanel();
+            } catch (RemoteException e) {
+                // A RemoteException is like a cold
+                // Let's hope we don't catch one!
+            }
         }
     }
 
@@ -324,6 +354,59 @@ public class PieControlPanel extends FrameLayout implements StatusBarPanel,
             }
         }
     }
+
+    private void toggleLastApp() {
+        int lastAppId = 0;
+        int looper = 1;
+        String packageName;
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        final ActivityManager am = (ActivityManager) mContext
+                .getSystemService(Activity.ACTIVITY_SERVICE);
+        String defaultHomePackage = "com.android.launcher";
+        intent.addCategory(Intent.CATEGORY_HOME);
+        final ResolveInfo res = mContext.getPackageManager().resolveActivity(intent, 0);
+        if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
+            defaultHomePackage = res.activityInfo.packageName;
+        }
+        List <ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(5);
+        // lets get enough tasks to find something to switch to
+        // Note, we'll only get as many as the system currently has - up to 5
+        while ((lastAppId == 0) && (looper < tasks.size())) {
+            packageName = tasks.get(looper).topActivity.getPackageName();
+            if (!packageName.equals(defaultHomePackage) && !packageName.equals("com.android.systemui")) {
+                lastAppId = tasks.get(looper).id;
+            }
+            looper++;
+        }
+        if (lastAppId != 0) {
+            am.moveTaskToFront(lastAppId, am.MOVE_TASK_NO_USER_ACTION);
+        }
+    }
+
+    public static class KillTask implements Runnable {
+         private Context mContext;
+         public KillTask(Context context) {
+             this.mContext = context;
+         }
+         public void run() {
+            final Intent intent = new Intent(Intent.ACTION_MAIN);
+            final ActivityManager am = (ActivityManager) mContext
+                    .getSystemService(Activity.ACTIVITY_SERVICE);
+            String defaultHomePackage = "com.android.launcher";
+            intent.addCategory(Intent.CATEGORY_HOME);
+            final ResolveInfo res = mContext.getPackageManager().resolveActivity(intent, 0);
+            if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
+                defaultHomePackage = res.activityInfo.packageName;
+            }
+            String packageName = am.getRunningTasks(1).get(0).topActivity.getPackageName();
+            if (SysUIPackage.equals(packageName))
+                return; // don't kill SystemUI
+            if (!defaultHomePackage.equals(packageName)) {
+                am.forceStopPackage(packageName);
+                Toast.makeText(mContext, com.android.internal.R.string.app_killed_message, Toast.LENGTH_SHORT).show();
+            }
+        }
+     }
 
     public void injectKeyDelayed(int keycode) {
         mInjectKeycode = keycode;

packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
@@ -80,14 +80,16 @@ public class PieMenu extends FrameLayout {
     private static int ANIMATOR_END = ANIMATOR_SNAP_GROW;
 
     private static final int COLOR_OPAQUE_MASK = 0xff000000;
-    private static final int COLOR_SNAP_BACKGROUND = 0xffffffff;
-    private static final int COLOR_PIE_BACKGROUND = 0xaa000000;
+    private static final int COLOR_SNAP_BACKGROUND = 0xffffff;
+    private static final int COLOR_PIE_BACKGROUND = 0xaaff005e;
     private static final int COLOR_PIE_BUTTON = 0xb2ffffff;
-    private static final int COLOR_PIE_SELECT = 0xaaffffff;
+    private static final int COLOR_PIE_SELECT = 0xaadbff00;
     private static final int COLOR_PIE_OUTLINES = 0x55ffffff;
-    private static final int COLOR_CHEVRON_LEFT = 0x0999cc;
+    private static final int COLOR_CHEVRON_LEFT = 0xffffff;
     private static final int COLOR_CHEVRON_RIGHT = 0xffffff;
     private static final int COLOR_BATTERY_JUICE = 0xffffff;
+    private static final int COLOR_BATTERY_JUICE_LOW = 0xffbb33;
+    private static final int COLOR_BATTERY_JUICE_CRITICAL = 0xff4444;
     private static final int COLOR_BATTERY_BACKGROUND = 0xffffff;
     private static final int COLOR_STATUS = 0xffffff;
     private static final int BASE_SPEED = 500;
@@ -199,6 +201,11 @@ public class PieMenu extends FrameLayout {
     private boolean mEnableColor;
     private boolean mUseMenuAlways;
     private boolean mUseSearch;
+    private boolean mUseLastApp;
+    private boolean mUseKillTask;
+    private boolean mUseTorch;
+    private boolean mUseActNotif;
+    private boolean mUsePower;
     private boolean mHapticFeedback;
 
     // Animations
@@ -247,7 +254,17 @@ public class PieMenu extends FrameLayout {
         mUseMenuAlways = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PA_PIE_MENU, 1) == 1;
         mUseSearch = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.PA_PIE_SEARCH, 1) == 1;
+                Settings.System.PA_PIE_SEARCH, 0) == 1;
+        mUseLastApp = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PA_PIE_LAST_APP, 0) == 1;
+        mUseKillTask = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PA_PIE_KILL_TASK, 0) == 1;
+        mUseTorch = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PA_PIE_TORCH, 0) == 1;
+        mUseActNotif = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PA_PIE_ACT_NOTIF, 0) == 1;
+        mUsePower = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PA_PIE_POWER, 0) == 1;
         mStatusMode = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PA_PIE_MODE, 2);
         mPieSize = Settings.System.getFloat(mContext.getContentResolver(),
@@ -664,9 +681,14 @@ public class PieMenu extends FrameLayout {
     }
 
     private boolean canItemDisplay(PieItem item) {
-        return !(item.getName().equals(PieControl.MENU_BUTTON) && !mPanel.currentAppUsesMenu() && !mUseMenuAlways)
-                &&
-                !(item.getName().equals(PieControl.SEARCH_BUTTON) && !mUseSearch);
+        return !(item.getName().equals(PieControl.MENU_BUTTON) && !mPanel.currentAppUsesMenu()
+                        && !mUseMenuAlways) &&
+               !(item.getName().equals(PieControl.SEARCH_BUTTON) && !mUseSearch) &&
+               !(item.getName().equals(PieControl.ACT_NOTIF_BUTTON) && !mUseActNotif) &&
+               !(item.getName().equals(PieControl.TORCH_BUTTON) && !mUseTorch) &&
+               !(item.getName().equals(PieControl.KILL_TASK_BUTTON) && !mUseKillTask) &&
+               !(item.getName().equals(PieControl.LAST_APP_BUTTON) && !mUseLastApp) &&
+               !(item.getName().equals(PieControl.POWER_BUTTON) && !mUsePower);
     }
 
     private void layoutPie() {
@@ -678,6 +700,16 @@ public class PieMenu extends FrameLayout {
         if (!mPanel.currentAppUsesMenu() && !mUseMenuAlways)
             itemCount--;
         if (!mUseSearch)
+            itemCount--;
+        if (!mUseActNotif)
+            itemCount--;
+        if (!mUseLastApp)
+            itemCount--;
+        if (!mUseTorch)
+            itemCount--;
+        if (!mUseKillTask)
+            itemCount--;
+        if (!mUsePower)
             itemCount--;
 
         int lesserSweepCount = 0;
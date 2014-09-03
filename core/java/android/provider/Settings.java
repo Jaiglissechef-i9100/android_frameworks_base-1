DirtyUnicorns/android_frameworks_base

Screenshot Pie Target [1/2]
Change-Id: I16100c3f253ac91c00a8971f7cad21e36994a3f1

core/java/android/provider/Settings.java
@@ -4996,6 +4996,12 @@ public final class Settings {
         public static final String PIE_TORCH = "pie_torch";
 
         /**
+         * Pie screenshot
+         * @hide
+         */
+        public static final String PIE_SCREENSHOT = "pie_screenshot";
+
+        /**
          * Pie action notifications, default to off
          * @hide
          */

packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
@@ -204,6 +204,7 @@ public class PieMenu extends FrameLayout {
     private boolean mUseTorch;
     private boolean mUseActNotif;
     private boolean mUsePower;
+    private boolean mUseScreenshot;
     private boolean mHapticFeedback;
 
     // Animations
@@ -263,6 +264,8 @@ public class PieMenu extends FrameLayout {
                 Settings.System.PIE_ACT_NOTIF, 0) == 1;
         mUsePower = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PIE_POWER, 0) == 1;
+        mUseScreenshot = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_SCREENSHOT, 0) == 1;
         mStatusMode = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PIE_MODE, 2);
         mPieSize = Settings.System.getFloat(mContext.getContentResolver(),
@@ -686,7 +689,8 @@ public class PieMenu extends FrameLayout {
                !(item.getName().equals(PieControl.TORCH_BUTTON) && !mUseTorch) &&
                !(item.getName().equals(PieControl.KILL_TASK_BUTTON) && !mUseKillTask) &&
                !(item.getName().equals(PieControl.LAST_APP_BUTTON) && !mUseLastApp) &&
-               !(item.getName().equals(PieControl.POWER_BUTTON) && !mUsePower);
+               !(item.getName().equals(PieControl.POWER_BUTTON) && !mUsePower) &&
+               !(item.getName().equals(PieControl.SCREENSHOT_BUTTON) && !mUseScreenshot);
     }
 
     private void layoutPie() {
@@ -708,6 +712,8 @@ public class PieMenu extends FrameLayout {
         if (!mUseKillTask)
             itemCount--;
         if (!mUsePower)
+            itemCount--;
+        if (!mUseScreenshot)
             itemCount--;
 
         int lesserSweepCount = 0;

Jaiglissechef-i9100/android_frameworks_base-1

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java

core/java/android/provider/Settings.java
@@ -5348,6 +5348,12 @@ public final class Settings {
          */
         public static final String PA_PIE_POWER = "pa_pie_power";
 
+        /**
+         * Pie screenshot
+         * @hide
+         */
+        public static final String PA_PIE_SCREENSHOT = "pa_pie_screenshot";
+
        /**
          * HALO enabled, should default to 0 (HALO is disabled)
          * @hide

packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
@@ -206,6 +206,7 @@ public class PieMenu extends FrameLayout {
     private boolean mUseTorch;
     private boolean mUseActNotif;
     private boolean mUsePower;
+    private boolean mUseScreenshot;
     private boolean mHapticFeedback;
 
     // Animations
@@ -265,6 +266,8 @@ public class PieMenu extends FrameLayout {
                 Settings.System.PA_PIE_ACT_NOTIF, 0) == 1;
         mUsePower = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PA_PIE_POWER, 0) == 1;
+        mUseScreenshot = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PA_PIE_SCREENSHOT, 0) == 1;
         mStatusMode = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PA_PIE_MODE, 2);
         mPieSize = Settings.System.getFloat(mContext.getContentResolver(),
@@ -688,7 +691,8 @@ public class PieMenu extends FrameLayout {
                !(item.getName().equals(PieControl.TORCH_BUTTON) && !mUseTorch) &&
                !(item.getName().equals(PieControl.KILL_TASK_BUTTON) && !mUseKillTask) &&
                !(item.getName().equals(PieControl.LAST_APP_BUTTON) && !mUseLastApp) &&
-               !(item.getName().equals(PieControl.POWER_BUTTON) && !mUsePower);
+               !(item.getName().equals(PieControl.POWER_BUTTON) && !mUsePower) &&
+               !(item.getName().equals(PieControl.SCREENSHOT_BUTTON) && !mUseScreenshot);
     }
 
     private void layoutPie() {
@@ -710,6 +714,8 @@ public class PieMenu extends FrameLayout {
         if (!mUseKillTask)
             itemCount--;
         if (!mUsePower)
+            itemCount--;
+        if (!mUseScreenshot)
             itemCount--;
 
         int lesserSweepCount = 0;